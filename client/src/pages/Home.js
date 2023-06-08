341be7941e3057a7108c78c6cda91f3a48151fba
Julia B <julia.bernst@gmail.com>
2023-06-08 16:11:45 -0400

    minor fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,8 +12,6 @@ export default function Home() {
         <Navbar />
         <div className="home-div">
           {/* we can remove this, just here for movement to chat page */}
-          <p>temporary link:</p>
-          <Link to="/chats">go to chats</Link>
           <CreateChat />
           <Profile />
         </div>
